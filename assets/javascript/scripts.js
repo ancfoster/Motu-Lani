59c6d1f76ad0b335bce3abaf06b316dfec2322d2
Alexander Foster <a.foster@outlook.com>
2022-06-01 12:09:07 +0100

    Add comments to script.js & clean up code

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -4,32 +4,53 @@ let y = new Date();
 document.getElementById("year").innerHTML = y.getFullYear();
 // Sets initial status of responsive menu
 let dropStatus = 0;
+// Initialize variabless
+let burgerTop = document.getElementById("burger-top");
+let burgerMiddle = document.getElementById("burger-middle");
+let burgerBottom = document.getElementById("burger-bottom");
+let mobileNavCont = document.getElementById("mobile-navigation-container")
 // Controls burger icon animation and opens/closes mobile navigation
 function navDrop() {
-    if (dropStatus == 0) {
-	dropStatus ++;
-    document.getElementById("burger-middle").style.opacity = "0";
-	document.getElementById("burger-top").style.width = "40px";
-	document.getElementById("burger-bottom").style.width = "40px";
-	document.getElementById("burger-top").style.Transform = "rotate(45deg) translate(7px,7px)";
-	document.getElementById("burger-top").style.WebkitTransform = "rotate(45deg) translate(7px,7px)";
-    document.getElementById("burger-bottom").style.Transform = "rotate(-45deg) translate(7px,-7px)";
-	document.getElementById("burger-bottom").style.WebkitTransform = "rotate(-45deg) translate(7px,-7px)";
-    document.getElementById("mobile-navigation-container").style.WebkitTransform = "translateY(100%)"; 
-	document.getElementById("mobile-navigation-container").style.Transform = "translateY(100%)"; 
+	if (dropStatus == 0) {
+		dropStatus ++;
+	burgerMiddle.style.cssText = `
+	opacity: 0;
+	`;
+	burgerTop.style.cssText = `
+	width: 40px;
+	transform: rotate(45deg) translate(7px,7px);
+	-webkit-transform: rotate(45deg) translate(7px,7px);
+	`;
+	burgerBottom.style.cssText = `
+	width: 40px;
+	transform: rotate(-45deg) translate(7px,-7px);
+	-webkit-transform: rotate(-45deg) translate(7px,-7px);
+	`;
+	mobileNavCont.style.cssText = `
+	transform: translateY(-00%);
+	-webkit-transform: translateY(100%);
+	`;
 	}
-	else {	
-	document.getElementById("mobile-navigation-container").style.WebkitTransform = "translateY(-100%)"; 
-	document.getElementById("mobile-navigation-container").style.Transform = "translateY(-100%)"; 
-	document.getElementById("burger-top").style.Transform = "rotate(0deg) translate(0px,0px)";
-	document.getElementById("burger-top").style.WebkitTransform = "rotate(0deg) translate(0px,0px)";
-	document.getElementById("burger-bottom").style.Transform = "rotate(0deg) translate(0px,0px)";
-	document.getElementById("burger-bottom").style.WebkitTransform = "rotate(0deg) translate(0px,0px)";
-	document.getElementById("burger-middle").style.opacity = "1.0";
+else {
+	mobileNavCont.style.cssText = `
+	transform: translateY(-100%);
+	-webkit-transform: translateY(-100%);
+	`;
+	burgerMiddle.style.cssText = `
+	opacity: 1.0;
+	`;
+	burgerTop.style.cssText = `
+	transform: rotate(0deg) translate(0px,0px);
+	-webkit-transform: rotate(0deg) translate(0px,0px);
+	`;
+	burgerBottom.style.cssText = `
+	transform: rotate(0deg) translate(0px,0px);
+	-webkit-transform: rotate(0deg) translate(0px,0px);
+	`;
 	dropStatus --;
-	}    
-};
-
+	}
+}
+// Listens for click on burger icon
 document.getElementById("burger-icon-container").addEventListener("click", function(){navDrop();});
 
 
